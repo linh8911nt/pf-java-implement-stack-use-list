8ab72dbd89056b387eed045990775be2b41619b9
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-05 10:49:05 +0700

    implement stack use list

---
+++ MyStack.java
@@ -0,0 +1,43 @@
+public class MyStack {
+    private int size;
+    private int[] arr;
+    private int index = 0;
+
+    public MyStack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public void push(int element){
+        if (isFull()){
+            throw new StackOverflowError("Stack is full!");
+        }
+        arr[index] = element;
+        index++;
+    }
+
+    public int pop() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Stack empty!");
+        }
+        return arr[--index];
+    }
+
+    public boolean isEmpty(){
+        if (index == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull(){
+        if (index == size){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return index;
+    }
+}
